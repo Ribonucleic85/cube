9d1430fb405afaef1c60f4ba0feb469e6c1d3e0b
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2019-06-07 02:11:32 +0100

    Update alg_translator.js

--- alg_translator.js
+++ alg_translator.js
@@ -1,20 +1,13 @@
 Array.prototype.rightRot = function() { this.unshift(this.pop()); return this; }
 Array.prototype.leftRot  = function() { this.push(this.shift()); return this; }
 Array.prototype.inv = function(){ this[2]+="'"; this[3]+="'"; }
-const Lock = function(){
-  var al = 0;
-  for (; al<arguments.length; al++)
-    Object.freeze(arguments[al]);
-}
 
-/* modify Lock() with this type of loop also could be used to lessen needed calls
-   also mod inv() to shorten the number of calls needed for it
-(function() {
-  for (let arg of arguments) {
-    console.log(arg);
-  }
-})(1, 2, 3);
-*/
+// except for the prototypes this code is untested, use at your own risk
+
+const Lock = function(){
+  for (let al of arguments)
+    Object.freeze(al);
+}
 
 //    x,y,z = 90 deg
 // xi,yi,zi = 90 deg rev
@@ -38,27 +31,50 @@ Lock(
   Midd, Midd["x"], Midd["y"], Midd["z"], Midd["x'"], Midd["y'"], Midd["z'"]
 );
 
-
 trans = (alg, ax)=>{
-  var ac, ed, mi, edsh,  edmi;
+  var /*cm*/ ccw, ed, mi, edsh, mish; // cm is dealt with in for..of loops
 
   alg = alg.split(" ");
-  ac = 0, // loop counter for alg reconstruction
+  ccw = !1; // is move ccw?
 
-  ed = Edge[ax],
-  mi = Midd[ax],
-  edsh = Edge[ax],
-  edmi = Midd[ax];
+  ed   = [...Edge[ax]];
+  mi   = [...Midd[ax]];
+  edsh = [...Edge[ax]];
+  mish = [...Midd[ax]];
   edsh.rightRot();
-  edmi.rightRot();
+  mish.rightRot();
 
-  for (; ac<alg.length; ac++){
-    // check if inverted move first
+  for (let cm of alg) {
+    // ccw move?
+    if (cm.indexOf("'") != -1)
+      ccw = !0, // move is ccw
+      cm = cm[0]; // convert move to cw
 
-    // check if move is an outer layer move
-    if (ed.indexOf(alg[ac])>-1) // not finished
+    // edge move?
+    if (ed.indexOf(cm) != -1) {
+      cm = ed[edsh.indexOf(cm)]; // is edge move so transform it to next move in the sequence
+      cm += ccw? "'": ""; // if move was inverted change it back
+      ccw = !1; // restore ccw indicator to false
+      continue;
+    }
+
+    // middle move?
+    if (mi.indexOf(cm) != -1) {
+      cm += ccw? "'": "";
+      ccw = !1;
+      cm = mi[mish.indexOf(cm)]; // is middle move so transform it to next move in the sequence
+      continue;
+    }
+    
+    // if no match is found then the move doesn't need to change
+    // for example in a y transform L, R and M moves are not changed
   }
+
+  alg.push(""); // add blank entry, this will hold the reconstructed array
+
+  while (alg.length>1) {
+    alg[alg.length-1] += alg.shift() + (alg.length>2? " ": "");
+  }
+  
+  return alg[0]; // if we returned alg then it would be a single item array
 }
-
-
-
